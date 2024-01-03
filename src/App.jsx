e8eb7202b31717420631946eac4534e58eb56945
Miguel <aries_242015@hotmail.com>
2024-01-03 16:58:26 -0500

    agrega componente de Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import { Aside } from "./components/Aside";
 import { ModalPokemon } from "./components/ModalPokemon";
 import { Pokemons } from "./components/Pokemons";
 import { PokemonContext } from "./context/PokemonContext";
+import { Header } from "./components/Header";
 
 function App() {
-  const { showDetailPokemon, closePokemonDetail, pokemonDetail, isLoadingPokemon } =
-    useContext(PokemonContext);
+  const {
+    showDetailPokemon,
+    closePokemonDetail,
+    pokemonDetail,
+    isLoadingPokemon,
+  } = useContext(PokemonContext);
 
   return (
     <>
       <section className="parent-container">
+        <Header/>
         <main>
           <Pokemons />
           <Aside pokemon={pokemonDetail} isLoadingPokemon={isLoadingPokemon} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="parent-header">
+      <div className="header">
+        <h1>PokedexWeb</h1>
+        <a href="https://github.com/MrDevv" target="_blank">
+          <i class="fa-brands fa-github"></i>
+        </a>
+      </div>
+    </header>
+  );
+};
